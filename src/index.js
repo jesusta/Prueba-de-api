e43ecca4c5e63e1de0d14ac01ee0fa252af02f50
Jesus <jesus.diaz@unisimon.edu.co>
2021-10-26 22:12:20 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const express= require('express');
 const app=express();
 const morgan= require('morgan');
+const path = require('path');
 
 //settings
-app.set('port',800)
+app.set('port',process.env.PORT || 800);
 
 //middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:flase}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//router
+app.get("/",(req,res)=>{
+    res.render(path.join(__dirname+'/index.ejs'));
+});
 
 
 //inicia serve
 app.listen(app.get('port'),()=>{
     console.log(`Sever en port ${app.get('port')}`)
-})
+});
